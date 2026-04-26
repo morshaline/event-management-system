d5e720d6488c84761630b9418bf5ae50c858f8be
morshaline <156164966+morshaline@users.noreply.github.com>
2026-04-26 17:07:18 +0600

    Delete frontend directory

--- frontend/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/event-management-system/', // এখানে আপনার গিটহাব রিপোজিটরির নাম ঠিক এভাবেই দেবেন
-})
